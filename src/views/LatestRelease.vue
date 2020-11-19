6df9bb8a52da5bdbedd9c17c3f4a7361c4191e22
Sergio <sergioivanprudencio@gmail.com>
2020-11-19 19:18:11 -0400

    latest release

--- src/views/LatestRelease.vue
+++ src/views/LatestRelease.vue
@@ -1,17 +1,41 @@
 <template>
-    <div>
-        <NavBar/>
-    </div>
+  <div>
+    <NavBar />
+    <v-row no-gutters>
+      <v-col v-for="(game,index) in gameInfo" :key="index" cols="12" sm="6">
+        <LatestGameCard
+              :title="game.title"
+              :price="game.price"
+              :id="game.id"
+              :color="game.color"
+              :banner="game.banner"
+              :description="game.description"
+            />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-
-import NavBar from '../components/generic/NavBar.vue'
+import NavBar from "../components/generic/NavBar.vue";
+import LatestGameCard from "@/components/latest/LatestGameCard.vue";
+import axios from "axios";
 
 export default {
-  name: 'LatestRelease',
+  name: "LatestRelease",
   components: {
-    NavBar
-  }
-}
+    NavBar,
+    LatestGameCard
+  },
+  data() {
+    return {
+      gameInfo: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8080/ming/v1/games?page=1&latest=true")
+      .then((response) => (this.gameInfo = response.data));
+  },
+};
 </script>
