6a98fed3283dfdade8027ba63c1b94ca37e14eb1
Martin Campuzano <martin.campuzano.flores@gmail.com>
2020-11-18 21:55:35 -0400

    Fixed routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import HomePage from '@/views/HomePage.vue'
 
 Vue.use(VueRouter)
@@ -9,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: HomePage
   },
   {
     path: '/about',
@@ -18,11 +17,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/HomePage',
-    name: 'HomePage',
-    component: HomePage,
   },
 ]
 
