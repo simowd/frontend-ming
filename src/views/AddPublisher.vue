728c21e4f7f3b429816709e925998d48cfe016aa
JuancaJcA <avilafloresjc@gmail.com>
2020-11-19 18:24:34 -0400

    Sign Up NavBar addded

---
+++ src/views/AddPublisher.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <NavBarAdmin/>
+    </div>
+</template>
+
+<script>
+import NavBarAdmin from '@/components/generic/NavBarAdmin.vue'
+export default {
+    name: "AddPublisher",
+    components:{
+        NavBarAdmin
+    }
+}
+</script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <NavBar />
+        <div class="card-auth">
+
+        </div>
     </div>    
 </template>
 
