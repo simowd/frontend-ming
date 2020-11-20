afab6fdf66246b7dd79b9c8887f662f48a026a90
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-11-19 20:35:01 -0400

    v-for added to Carousel.vue

--- src/components/sale/Carousel.vue
+++ src/components/sale/Carousel.vue
@@ -1,11 +1,8 @@
 <template>
-    <div>
-        <carousel-3d>
-            <slide :index="0">
-                Slide 1 Content
-            </slide>
-            <slide :index="1">
-                Slide 2 Content
+    <div  id="sld">
+        <carousel-3d >
+            <slide class="card-sale-carousel" v-for="(slide, i) in slides" :index="i" :key="slide">
+                
             </slide>
         </carousel-3d>
     </div>
@@ -15,10 +12,17 @@
 import { Carousel3d, Slide } from 'vue-carousel-3d';
 
 export default {
+    el: '#sld',
+    data(){
+        return{
+            slides: 7
+        };
+        
+    },
     
     components: {
-        Carousel3d,
-        Slide
+    Carousel3d,
+    Slide
     }
 
 }
