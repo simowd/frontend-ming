a320838c5344ab9dc7305cc34e8ee02d51bfc378
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-11-19 22:38:26 -0400

    Carousel working with backend sort of

--- src/components/sale/Carousel.vue
+++ src/components/sale/Carousel.vue
@@ -1,33 +1,43 @@
 <template>
-    <div  id="sld">
-        <carousel-3d >
-            <slide class="card-sale-carousel" v-for="(slide, i) in slides" :index="i" :key="slide">
-                
-            </slide>
-        </carousel-3d>
-    </div>
+  <div id="sld">
+    <carousel-3d>
+      <slide
+        class="card-sale-carousel"
+        v-for="(slide, i) in gameInfo"
+        :index="i"
+        :key="slide" >
+        <figure>
+          <img v-bind:src="slide.banner"/>
+        </figure>
+      </slide>
+    </carousel-3d>
+  </div>
 </template>
 
 <script>
-import { Carousel3d, Slide } from 'vue-carousel-3d';
+import { Carousel3d, Slide } from "vue-carousel-3d";
+import axios from "axios";
 
 export default {
-    el: '#sld',
-    data(){
-        return{
-            slides: 7
-        };
-        
-    },
-    
-    components: {
-    Carousel3d,
-    Slide
-    }
+  el: "#sld",
+  data() {
+    return {
+      slides: 7,
+      gameInfo: null,
+    };
+  },
 
-}
+  components: {
+    Carousel3d,
+    Slide,
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:8080/ming/v1/games?page=1&sale=true")
+      .then((response) => (this.gameInfo = response.data));
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/sale/SearchBarSale.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="ofertas-label-container">
+      <v-row no-gutters>
+        <v-col cols="6">
+          <p>
+            Ofertas
+          </p>
+        </v-col>
+        <v-col cols="6">
+          
+            <div class="search-bar">
+              <v-text-field
+                label="Buscar en Ofertas"
+                outlined
+                color="#707070"
+                append-icon="mdi-magnify"
+                dense
+              >
+              </v-text-field>
+            </div>
+          
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBarVue",
+};
+</script>
+
+<style scoped>
+.v-text-field--outlined >>> fieldset {
+  border-color: #707070;
+}
+</style>
