f1df0cbf75d2dd356d9c0ec1b2388d783fff3d30
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-11-21 21:31:07 -0400

    Sale page.vue Complete jojojo

--- src/components/sale/Carousel.vue
+++ src/components/sale/Carousel.vue
@@ -2,25 +2,34 @@
   <div id="sld">
     <carousel>
       <slide v-for="(slide, i) in gameInfo" :index="i" :key="slide">
-        <figure>
-          <img class="card-sale-carousel" v-bind:src="slide.banner" />
+        <figure >
+          <img
+            class="card-sale-carousel"
+            v-bind:src="slide.banner"
+            :style="{
+          boxShadow: '0 1px 2rem ' + slide.color + ''
+        }"
+          />
         </figure>
-        <div >
+        <div>
           <v-row no-gutters>
             <v-col cols="6">
-              <div>
-                <p>
-                  {{ slide.title }}<br />
+              <div >
+                <p class="carousel-title">{{ slide.title }} <br /></p>
+                <div class="card-carousel-data">
+                  <p>
                   ${{
                     (slide.price - (slide.price * slide.sale) / 100).toFixed(2)
                   }}
-                  USD
+                  USD -
                 </p>
                 <p class="text-sale">${{ slide.price }} USD</p>
+                </div>
+                
               </div>
             </v-col>
             <v-col cols="6">
-              <div>- {{ slide.sale }} %</div>
+              <div class="carousel-discount">- {{ slide.sale }} %</div>
             </v-col>
           </v-row>
         </div>
@@ -32,6 +41,7 @@
 <script>
 import { Carousel, Slide } from "vue-carousel";
 import axios from "axios";
+import { URLBACKEND } from "@/assets/url.js";
 
 export default {
   el: "#sld",
@@ -49,7 +59,7 @@ export default {
 
   mounted() {
     axios
-      .get("http://localhost:8080/ming/v1/games?page=1&sale=true")
+      .get("http://" + URLBACKEND + "/ming/v1/games?page=1&sale=true")
       .then((response) => (this.gameInfo = response.data));
   },
 };
@@ -58,5 +68,24 @@ export default {
 <style scoped>
 .text-sale {
   text-decoration: line-through;
+}
+
+.carousel-title{
+  font-size: 2rem;
+  margin-top: 1rem;
+  text-align: right;
+  font-weight: 200;
+  margin-right: 3rem;
+}
+
+.carousel-discount{
+  font-size: 3rem;
+  text-align: center;
+  margin-top: 1.5rem;
+  margin-left: 5rem;
+  border-color: green;
+  border-style: solid;
+  color: green;
+  width: 25%;
 }
 </style>
