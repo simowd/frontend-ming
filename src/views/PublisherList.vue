eecc3a05f057755bd6ab1da5094fefacd3569a92
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-11-25 12:09:05 -0400

    PublisherCardList.vue created

--- src/views/PublisherList.vue
+++ src/views/PublisherList.vue
@@ -2,6 +2,18 @@
     <div class="background-image">
     <NavBarAdmin />
     
+    <div class="search-bar-publisher" >
+              <v-text-field
+                label="Buscar en Editores"
+                outlined
+                v-model="title"
+                color="#707070"
+                append-icon="mdi-magnify"
+                dense
+              >
+              </v-text-field>
+            </div>
+
   </div>
 </template>
 
@@ -17,6 +29,12 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style scoped>
+
+.search-bar-publisher{
+    width: 60rem;
+    margin-left: 102rem;
+    margin-top: 5rem;
+}
 
 </style>
