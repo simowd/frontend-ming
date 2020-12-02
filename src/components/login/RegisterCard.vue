91e6bc854a924e4add38d35c03aad23a684fb051
JuancaJcA <avilafloresjc@gmail.com>
2020-12-01 20:10:29 -0400

    Validations added in register

--- src/components/login/RegisterCard.vue
+++ src/components/login/RegisterCard.vue
@@ -14,81 +14,83 @@
           <v-layout justify-center align-center>
             <v-form>
               <v-container class="form-register">
-                <v-text-field
-                  outlined
-                  :rules="[rules.required]"
-                  label="Nombre Completo"
-                  dense
-                  color="#66698C"
-                  class="login-input"
-                  id="name"
-                  v-model="infoUser.name"
-                >
-                </v-text-field>
+                <v-form v-model="valid">
+                  <v-text-field
+                    outlined
+                    :rules="[rules.required]"
+                    label="Nombre Completo"
+                    dense
+                    color="#66698C"
+                    class="login-input"
+                    id="name"
+                    v-model="infoUser.name"
+                  >
+                  </v-text-field>
 
-                <v-text-field
-                  outlined
-                  :rules="[rules.required]"
-                  label="Apellidos"
-                  dense
-                  color="#66698C"
-                  class="login-input"
-                  id="lastname"
-                  v-model="infoUser.lastname"
-                >
-                </v-text-field>
+                  <v-text-field
+                    outlined
+                    :rules="[rules.required]"
+                    label="Apellidos"
+                    dense
+                    color="#66698C"
+                    class="login-input"
+                    id="lastname"
+                    v-model="infoUser.lastname"
+                  >
+                  </v-text-field>
 
-                <v-text-field
-                  outlined
-                  :rules="[rules.required]"
-                  label="Nombre de Usuario"
-                  dense
-                  color="#66698C"
-                  class="login-input"
-                  id="username"
-                  v-model="infoUser.username"
-                >
-                </v-text-field>
+                  <v-text-field
+                    outlined
+                    :rules="[rules.required]"
+                    label="Nombre de Usuario"
+                    dense
+                    color="#66698C"
+                    class="login-input"
+                    id="username"
+                    v-model="infoUser.username"
+                  >
+                  </v-text-field>
 
-                <v-text-field
-                  outlined
-                  :rules="[rules.required]"
-                  label="Apodo"
-                  dense
-                  color="#66698C"
-                  class="login-input"
-                  id="alias"
-                  v-model="infoUser.alias"
-                >
-                </v-text-field>
+                  <v-text-field
+                    outlined
+                    :rules="[rules.required]"
+                    label="Apodo"
+                    dense
+                    color="#66698C"
+                    class="login-input"
+                    id="alias"
+                    v-model="infoUser.alias"
+                  >
+                  </v-text-field>
 
-                <v-text-field
-                  outlined
-                  :rules="[rules.required, rules.email]"
-                  label="Correo Electrónico"
-                  dense
-                  color="#66698C"
-                  class="login-input"
-                  id="email"
-                  v-model="infoUser.email"
-                >
-                </v-text-field>
+                  <v-text-field
+                    outlined
+                    :rules="[rules.required, rules.email]"
+                    label="Correo Electrónico"
+                    dense
+                    color="#66698C"
+                    class="login-input"
+                    id="email"
+                    v-model="infoUser.email"
+                  >
+                  </v-text-field>
 
-                <v-text-field
-                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                  :rules="[rules.required, rules.min]"
-                  :type="show1 ? 'text' : 'password'"
-                  name="input-10-1"
-                  label="Contraseña"
-                  hint="Mínimo 8 caracteres"
-                  color="#66698C"
-                  outlined
-                  @click:append="show1 = !show1"
-                  class="login-input"
-                  dense
-                  id="password"
-                  v-model="infoUser.password"
-                ></v-text-field>
+                  <v-text-field
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Contraseña"
+                    hint="Mínimo 8 caracteres"
+                    color="#66698C"
+                    outlined
+                    @click:append="show1 = !show1"
+                    class="login-input"
+                    dense
+                    id="password"
+                    v-model="infoUser.password"
+                  ></v-text-field>
+                </v-form>
               </v-container>
               <v-layout justify-center align-center>
                 <v-btn
@@ -108,6 +110,12 @@
         </v-col>
         <v-col :cols="5">
           <img src="@/assets/fez-login.png" class="logo-auth" />
+          <v-alert dense outlined type="error" v-if="alert" width="45rem">
+            {{ this.alert_text }}
+          </v-alert>
+          <!-- <<v-alert dense outlined :type="alert_type" v-if="alert" width="45rem">
+            {{ this.alert_text }}
+          </v-alert>-->
         </v-col>
       </v-row>
     </v-container>
@@ -122,6 +130,9 @@ export default {
   name: "RegisterCard",
   data() {
     return {
+      valid: null,
+      alert: false,
+      alert_text: null,
       infoUser: {
         name: null,
         lastname: null,
@@ -149,22 +160,42 @@ export default {
         .then((response) => (this.infoUser = response.data));
     },
     verify() {
-      if (this.infoUser.name === null) {
-        alert("Falta el Nombre");
-      } else if (this.infoUser.lastname === null) {
-        alert("Falta el Apellido");
-      } else if (this.infoUser.username === null) {
-        alert("Falta el Nombre de Usuario");
-      } else if (this.infoUser.alias === null) {
-        alert("Falta el Apodo");
-      } else if (this.infoUser.email === null) {
-        alert("Falta el Email");
-      } else if (this.infoUser.password === null) {
-        alert("Falta la Contraseña");
+      // this.verifyAtrribute(this.infoUser.name, "Falta el Nombre");
+      if (this.checkProperties(this.infoUser)) {
+        this.alert_text = "Todos los campos deben ser rellenados.";
+        this.alert = true;
+        this.alertTime();
       } else {
-        alert("Usuario Registrado");
-        this.create();
+        if (this.valid) {
+          this.create();
+        } else {
+          this.alert_text = "El formulario no es válido.";
+          this.alert = true;
+          this.alertTime();
+        }
       }
+    },
+    alertTime() {
+      setTimeout(() => {
+        this.alert = false;
+      }, 3000);
+    },
+    // verifyAtrribute(attribute, text) {
+    //   this.alert_type = "error";
+    //   if (attribute === null) {
+    //     this.alert_text = text;
+    //     this.alert = true;
+    //     this.alertTime();
+    //   }
+    // },
+    checkProperties(obj) {
+      var flag = false;
+      for (var key in obj) {
+        if (obj[key] === null || obj[key] === "") {
+          flag = true;
+        }
+      }
+      return flag;
     },
   },
 };
